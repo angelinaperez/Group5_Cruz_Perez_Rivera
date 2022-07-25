ca2ff9855e84b53d9d054f2ae29548ab90bf1423
angelinaperez <107536815+angelinaperez@users.noreply.github.com>
2022-07-25 20:56:36 +0800

    Add files via upload
    /CrochetShop/ files contain the comments for codes

---
+++ CrochetShop/global_variable.py
@@ -0,0 +1,20 @@
+#minimum is the starting index to be displayed
+#maximum is the index number after the last index to be displayed
+#simply, indexes 0-9
+minimum = 0
+maximum = 10
+
+#data stores data from csv file and database
+data = []
+
+#searchdata stores filtered data depending on what is searched
+search_data = []
+
+#for util defError function
+input_error = False
+error_msg = ''
+
+#to check if user searched something or not
+search = False
+
+
